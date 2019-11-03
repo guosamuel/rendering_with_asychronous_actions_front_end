7b7cefd42714fad7bbeab175eec3aca165472a76
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-11-03 16:13:58 -0500

    able to render information from the fetch request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  state = {
+    user: null
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:4001/users/")
+      .then(resp => resp.json())
+      .then(user => {
+        this.setState({user: user})
+      })
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div>
-        <label>Hello World!</label>
+        <label>{this.state.user ? this.state.user.users[0].name : "THE ASYNCHRONOUS FETCH HAS NOT BEEN COMPLETED YET"}</label>
       </div>
     );
   }
